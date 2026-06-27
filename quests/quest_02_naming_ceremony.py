0c9f168187daf0418fee01b415fbea670a79239a
fiinsikabore <f.kabore@alustudent.com>
2026-06-27 13:28:37 +0000

    Level 1 quests

---
+++ quests/quest_02_naming_ceremony.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+hero_name = "Aeloria"
+superpower = "Chrono-Shifting (Time Control)"
+
+print("Behold " + hero_name + ", a legendary guardian whose ultimate power is " + superpower + "!")
