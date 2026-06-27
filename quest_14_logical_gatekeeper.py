f12a81919299ec401e101a339a274a4643be9411
bouattara-cpu <b.ouattara@alustudent>
2026-06-27 11:56:48 +0200

    Quest 14: The Logical Gatekeeper

---
+++ quest_14_logical_gatekeeper.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+age = int(input("How old are you ?\n"))
+gold = int(input("How many gold coins do you have ?\n"))
+if age >= 18 and gold >= 20:
+    print("You can enter")
+else: 
+    print("You can not enter") 
