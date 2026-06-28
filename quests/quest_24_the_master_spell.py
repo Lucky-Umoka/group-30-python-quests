745c72e00e4cb3401ef4f5870e3ccb45cc9cc274
akagermain <g.iradukund2@alustudent.com>
2026-06-28 22:15:48 +0200

    Quest 24 modified, made second function for validating age

--- quests/quest_24_the_master_spell.py
+++ quests/quest_24_the_master_spell.py
@@ -6,3 +6,6 @@ def ask_for_age():
 def can_they_vote(age):
     if age >= 18:
         print(f"At {age} years old, you are eligible to vote!")
+    else:
+        print(f"At {age} years old, you are not eligible to vote. You have {18 - age} more year(s) to go.")
+
