28d5f24aeb12e274bb37b64430b9d3c82c000544
akagermain <g.iradukund2@alustudent.com>
2026-06-28 22:12:02 +0200

    Modify quest 24, started working on function two

--- quests/quest_24_the_master_spell.py
+++ quests/quest_24_the_master_spell.py
@@ -2,3 +2,7 @@
 def ask_for_age():
     age = int(input('Please enter your age: '))
     return age
+
+def can_they_vote(age):
+    if age >= 18:
+        print(f"At {age} years old, you are eligible to vote!")
